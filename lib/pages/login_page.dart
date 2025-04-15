d0c28034d4136846964c5b8d664899f22cdf2d33
Shreyansh <shrxx.acc@gmail.com>
2025-04-16 00:02:25 +0530

    Refactor code structure for improved readability and maintainability

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
+  static get context => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,12 +28,21 @@ class LoginPage extends StatelessWidget {
                     'Login',
                     style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                   ),
-                  const SizedBox(width: 48), // Placeholder for the icon
+                  SizedBox(width: 48),
                 ],
               ),
             ),
+            const SizedBox(height: 125),
+            CircleAvatar(
+              backgroundImage: const AssetImage('assets/images/logo/logo.png'),
+              maxRadius: 55,
+              minRadius: 50,
+            ),
             const SizedBox(height: 20),
-            _buildLoginButton(),
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: _buildLoginForm(),
+            ),
           ],
         ),
       ),
@@ -39,21 +50,44 @@ class LoginPage extends StatelessWidget {
   }
 }
 
-Widget _buildLoginButton() {
+Widget _buildLoginForm() {
   return Column(
     children: [
+      _buildTextField('Email', false),
+      SizedBox(height: 20),
+      _buildTextField('Password', true),
+      SizedBox(height: 20),
       SizedBox(
-        width: 300,
-        child: TextFormField(
-          decoration: InputDecoration(
-            labelText: 'Email',
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15.0),
-              borderSide: BorderSide(color: AppColor.brand500),
-            ),
-          ),
-        ),
+        width: double.maxFinite,
+        child: _buildLoginButton('Login', AppColor.brand500),
       ),
     ],
+  );
+}
+
+Widget _buildTextField(String label, bool isPassword) {
+  return TextFormField(
+    obscureText: isPassword,
+    decoration: InputDecoration(
+      labelText: label,
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(15.0),
+        borderSide: BorderSide(color: AppColor.brand500),
+      ),
+    ),
+  );
+}
+
+Widget _buildLoginButton(String label, Color color) {
+  return ElevatedButton(
+    onPressed: () {
+      // Handle login action
+    },
+    style: ElevatedButton.styleFrom(
+      backgroundColor: color,
+      padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+    ),
+    child: Text(label, style: TextStyle(fontSize: 18)),
   );
 }
