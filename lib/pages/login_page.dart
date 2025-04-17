89ef453c1df67e2aaf2edf7fa78583e861bb631f
Shreyansh <shrxx.acc@gmail.com>
2025-04-17 13:26:23 +0530

    Add Google logo image asset to pubspec.yaml and include the image file

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,93 +1,213 @@
+import 'package:app_web_v1/services/firebase_auth.dart';
 import 'package:app_web_v1/utilities/colors.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
-  static get context => null;
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
 
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(height: 50),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.arrow_back),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+              _buildHeader(context, 'Login'),
+              const SizedBox(height: 120),
+              const CircleAvatar(
+                backgroundImage: AssetImage('assets/images/logo/logo.png'),
+                maxRadius: 55,
+                minRadius: 50,
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25, 0, 25, 0),
+                child: _buildLoginForm(),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'By continuing, you agree to our Terms of Service and Privacy Policy',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+              const SizedBox(height: 15),
+              _buildDivider(),
+              const SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25, 0, 25, 0),
+                child: InkWell(
+                  onTap: () {
+                    AuthMethod().signinwithGoogle(context);
+                  },
+                  child: Container(
+                    height: 50,
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(color: Colors.grey, width: 1),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/google_logo.png',
+                          height: 30,
+                        ),
+                        const SizedBox(width: 5),
+                        Text(
+                          'Continue with Google',
+                          style: Theme.of(
+                            context,
+                          ).textTheme.labelLarge?.copyWith(
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  const Text(
-                    'Login',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(width: 48),
-                ],
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  // Handle forgot password action
+                },
+                child: Text(
+                  'Forgot Password?',
+                  style: TextStyle(color: AppColor.brand500),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.fromLTRB(25, 0, 25, 40),
+        child: InkWell(
+          onTap: () {
+            setState(() {
+              // REGISTERATION PAGE
+            });
+          },
+          child: Container(
+            height: 50,
+            width: double.infinity,
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: AppColor.brand500, width: 1),
+            ),
+            child: Text(
+              'Create an account',
+              style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                fontWeight: FontWeight.w600,
+                color: AppColor.brand500,
               ),
             ),
-            const SizedBox(height: 125),
-            CircleAvatar(
-              backgroundImage: const AssetImage('assets/images/logo/logo.png'),
-              maxRadius: 55,
-              minRadius: 50,
-            ),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: _buildLoginForm(),
-            ),
-          ],
+          ),
         ),
       ),
     );
   }
-}
 
-Widget _buildLoginForm() {
-  return Column(
-    children: [
-      _buildTextField('Email', false),
-      SizedBox(height: 20),
-      _buildTextField('Password', true),
-      SizedBox(height: 20),
-      SizedBox(
-        width: double.maxFinite,
-        child: _buildLoginButton('Login', AppColor.brand500),
+  Widget _buildHeader(BuildContext context, String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          Text(
+            title,
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(width: 48), // Placeholder for alignment
+        ],
       ),
-    ],
-  );
-}
+    );
+  }
 
-Widget _buildTextField(String label, bool isPassword) {
-  return TextFormField(
-    obscureText: isPassword,
-    decoration: InputDecoration(
-      labelText: label,
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(15.0),
-        borderSide: BorderSide(color: AppColor.brand500),
+  Widget _buildLoginForm() {
+    return Column(
+      children: [
+        _buildTextField('Email', false),
+        const SizedBox(height: 20),
+        _buildTextField('Password', true),
+        const SizedBox(height: 20),
+        _buildActionButton('Login', AppColor.brand500, () {
+          // Handle login action
+        }),
+      ],
+    );
+  }
+
+  Widget _buildTextField(String label, bool isPassword) {
+    return TextFormField(
+      obscureText: isPassword,
+      decoration: InputDecoration(
+        labelText: label,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15.0),
+          borderSide: BorderSide(color: AppColor.brand500),
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 
-Widget _buildLoginButton(String label, Color color) {
-  return ElevatedButton(
-    onPressed: () {
-      // Handle login action
-    },
-    style: ElevatedButton.styleFrom(
-      backgroundColor: color,
-      padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-    ),
-    child: Text(label, style: TextStyle(fontSize: 18)),
-  );
+  Widget _buildActionButton(String label, Color color, VoidCallback onPressed) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color,
+          padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+        ),
+        child: Text(label, style: const TextStyle(fontSize: 18)),
+      ),
+    );
+  }
+
+  Widget _buildDivider() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: Row(
+        children: [
+          Expanded(child: Divider(color: Colors.grey.shade300, thickness: 1)),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Text(
+              "OR",
+              style: TextStyle(
+                color: Colors.grey.shade600,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          Expanded(child: Divider(color: Colors.grey.shade300, thickness: 1)),
+        ],
+      ),
+    );
+  }
 }
